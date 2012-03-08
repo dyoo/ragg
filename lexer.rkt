a71f36ab64cfbe0900da24f1fe38cf97060d5528
Danny Yoo <dyoo@cs.wpi.edu>
2012-03-08 10:32:14 -0500

    minor testing

--- lexer.rkt
+++ lexer.rkt
@@ -1,9 +1,10 @@
 #lang racket/base
 (require parser-tools/lex
          (prefix-in : parser-tools/lex-sre)
-         "parser.rkt")
+         "parser.rkt"
+         rackunit)
 
-(provide (all-defined-out))
+(provide lex/1 tokenize)
 
 (define lex/1
   (lexer-src-pos
@@ -21,5 +22,28 @@
     (token-PIPE lexeme)]
    [(:or "+" "*")
     (token-REPEAT lexeme)]
+   [whitespace
+    (return-without-pos (lex/1 input-port))]
    [(eof)
     (token-EOF lexeme)]))
+
+;; tokenize: input-port -> (-> token)
+(define (tokenize ip)
+  (lambda ()
+    (lex/1 ip)))
+
+
+
+(define (l s)
+  (define t (lex/1 (open-input-string s)))
+  (list (token-name (position-token-token t))
+        (token-value (position-token-token t))
+        (position-offset (position-token-start-pos t))
+        (position-offset (position-token-end-pos t))))
+                             
+
+(check-equal? (l " hi")
+              '(ID "hi" 2 4))
+
+(check-equal? (l "  hi")
+              '(ID "hi" 3 5))
