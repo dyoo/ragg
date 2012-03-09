1d2727b4da5f4b23f25bd31b3fec753b69806ef2
Danny Yoo <dyoo@cs.wpi.edu>
2012-03-09 18:32:46 -0500

    disabling the grammar again

--- compile-time.rkt
+++ compile-time.rkt
@@ -2,18 +2,11 @@
 
 (require (for-template racket/base)
          racket/list
-         racket/set)
+         racket/set
+         "stx-types.rkt")
 
 (provide rules-codegen
-         rule
-         id
-         lit
-         token
-         choice
-         repeat
-         maybe
-         seq)
-
+         (all-from-out "stx-types.rkt"))
 
 
 (define (rules-codegen stx)
@@ -228,16 +221,3 @@
          (loop v implicit explicit))])))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; These are just here to provide bindings for Check Syntax.
-;; Otherwise, we should never hit these, as the toplevel rules-codegen
-;; should eliminate all uses of these if it does the right thing.
-(define (rules stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (rule stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (id stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (lit stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (token stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (choice stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (repeat stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (maybe stx) (raise-syntax-error #f "Used out of context of rules" stx))
-(define (seq stx) (raise-syntax-error #f "Used out of context of rules" stx))
