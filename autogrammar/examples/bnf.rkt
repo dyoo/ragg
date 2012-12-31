646dc153754600df83157646342ef795d0014199
Danny Yoo <dyoo@hashcollision.org>
2012-12-30 16:43:38 -0800

    bnf translation roughly adapted from http://en.wikipedia.org/wiki/Backus%E2%80%93Naur_Form

---
+++ autogrammar/examples/bnf.rkt
@@ -0,0 +1,11 @@
+#lang autogrammar
+
+ <syntax> : <rule> | <rule> <syntax>
+ <rule>   : <opt-whitespace> "<" <RULE-NAME> ">" <opt-whitespace> "::=" 
+                 <opt-whitespace> <expression> <line-end>
+ <opt-whitespace> : " " <opt-whitespace> | ""  ## "" is empty string, i.e. no whitespace
+ <expression>     : <list> | <list> "|" <expression>
+ <line-end>       : <opt-whitespace> <EOL> | <line-end> <line-end>
+ <list>    : <term> | <term> <opt-whitespace> <list>
+ <term>    : <literal> | "<" <RULE-NAME> ">"
+ <literal> : '"' <TEXT> '"' | "'" <TEXT> "'"   ## actually, the original BNF did not use quotes
