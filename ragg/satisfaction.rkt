2a9655e3f4c5e800dced9d1f39b181b7b340bb6a
Danny Yoo <dyoo@hashcollision.org>
2013-01-01 00:37:02 -0800

    optimizations

--- ragg/satisfaction.rkt
+++ ragg/satisfaction.rkt
@@ -7,47 +7,58 @@
 ;; I can't get no... satisfaction.
 ;;
 ;; A small module to make sure a small constraint system can be satisfied.
+;;
+;; Small variation on topological sort where we need both AND and OR nodes.
 
 
-(struct node (type val yes? parents children) #:mutable)
+(struct node (type val yes? parents count-to-satisfy) #:mutable)
 ;; or nodes are satisfied if any of the children is satisfied.
 ;; and nodes are satisfied if all of the children are satisfied.
 
 
 ;; visit!: node -> void
-;; Visit a node, and marking it if it's all satisfied.
-(define (visit! a-node)
-  (unless (node-yes? a-node)
-    (match a-node
-      [(node 'or val yes? parents children)
-       (when (for/or ([c (in-list children)]) (node-yes? c))
-         (set-node-yes?! a-node #t)
-         (for ([p (in-list parents)])
-           (visit! p)))]
-      [(node 'and val yes? parents children)
-       (when (for/and ([c (in-list children)]) (node-yes? c))
-         (set-node-yes?! a-node #t)
-         (for ([p (in-list parents)])
-           (visit! p)))])))
+;; Visit a node, and marking it if it's all satisfied.  Propagate
+;; satisfaction to parents as appropriate.
+(define visit!
+  (let ()
+    (define (dec! n)
+      (set-node-count-to-satisfy! n (max 0 (sub1 (node-count-to-satisfy n))))
+      (when (and (not (node-yes? n))
+                 (= (node-count-to-satisfy n) 0))
+        (sat! n)))
+    
+    (define (sat! n)
+      (set-node-yes?! n #t)
+      (for ([p (in-list (node-parents n))])
+        (dec! p)))
+    
+    (lambda (n)
+      (unless (node-yes? n)
+        (when (= (node-count-to-satisfy n) 0)
+          (sat! n))))))
 
 
 ;; make-or: X -> node
 ;; Create an or node
 (define (make-or val)
-  (node 'or val #f '() '()))
+  (node 'or val #f '() 1))
 
 
 ;; make-and: X -> node
 ;; Create an and node
 (define (make-and val)
-  (node 'and val #f '() '()))
+  (node 'and val #f '() 0))
+
 
 ;; add-child!: node node -> void
 ;; Attach a child c to the parent node p.
 (define (add-child! p c)
-  (set-node-children! p (cons c (node-children p)))
-  (set-node-parents! c (cons p (node-parents c))))
-
+  (set-node-parents! c (cons p (node-parents c)))
+  (match p
+    [(node 'or _ _ _ count-to-satisfy)
+     (void)]
+    [(node 'and _ _ _ count-to-satisfy)
+     (set-node-count-to-satisfy! p (add1 count-to-satisfy))]))
 
 
 (module* test racket
@@ -55,6 +66,7 @@
            racket/block
            rackunit)
   
+  ;; a ::= a
   (block
    ;; Self-looping "a" and-node should not say yes after visiting.
    (define a (make-and 'a))
@@ -62,6 +74,8 @@
    (visit! a)
    (check-false (node-yes? a)))
   
+  
+  ;; a ::= a
   (block
    ;; Self-looping "a" or-node should not say yes after visiting.
    (define a (make-or 'a))
@@ -69,16 +83,71 @@
    (visit! a)
    (check-false (node-yes? a)))
   
+  
+  ;; This case should never happen in my situation, but we should check.
   (block
    ;; Empty "a" or-node should not say yes after visiting.
    (define a (make-or 'a))
    (visit! a)
    (check-false (node-yes? a)))
   
+  
+  ;; a : TOKEN
   (block
    ;; Empty "a" and-node SHOULD say yes after visiting.
    (define a (make-and 'a))
    (visit! a)
    (check-true (node-yes? a)))
+
+  
+  ;; a : a | b
+  ;; b : TOKEN
+  (block
+   (define a (make-or 'a))
+   (add-child! a a)
+   (define b (make-and 'b))
+   (add-child! a b)
+   (visit! b)
+   (check-true (node-yes? a))
+   (check-true (node-yes? b)))
+  
+  ;; a : a b
+  ;; b : TOKEN
+  (block
+   (define a (make-and 'a))
+   (define b (make-and 'b))
+   (define TOKEN (make-and 'TOKEN))
+   (add-child! a a)
+   (add-child! a b)
+   (add-child! b TOKEN)
+   (visit! TOKEN)
+   (check-false (node-yes? a))
+   (check-true (node-yes? b))
+   (check-true (node-yes? TOKEN)))
+  
+  ;; a : b
+  ;; b : a
+  (block
+   (define a (make-and 'a))
+   (define b (make-and 'b))
+   (add-child! a b)
+   (add-child! b a)
+   (check-false (node-yes? a))
+   (check-false (node-yes? b)))
+  
+  ;; a : "a" b
+  ;; b : a | b
+  (block
+   (define a (make-and 'a))
+   (define b (make-or 'b))
+   (define lit (make-and "a"))
+   (add-child! a lit)
+   (add-child! a b)
+   (add-child! b a)
+   (add-child! b b)
+   (visit! lit)
+   (check-false (node-yes? a))
+   (check-false (node-yes? b))
+   (check-true (node-yes? lit)))
   
   )
