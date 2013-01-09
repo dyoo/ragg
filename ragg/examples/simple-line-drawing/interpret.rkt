b0a5145cd4557891da449830bb77371ac5e777c2
Danny Yoo <dyoo@hashcollision.org>
2013-01-09 16:05:14 -0700

    keeping the code synced up with the manual example

--- ragg/examples/simple-line-drawing/interpret.rkt
+++ ragg/examples/simple-line-drawing/interpret.rkt
@@ -1,25 +1,31 @@
 #lang racket/base
 (require syntax/parse)
 
-(provide interpret)
+(provide interpret-drawing)
 
-(define (interpret drawing-stx)
+(define (interpret-drawing drawing-stx)
   (syntax-parse drawing-stx
     [({~literal drawing} row-stxs ...)
 
      (for ([row-stx (syntax->list #'(row-stxs ...))])
        (interpret-row row-stx))]))
 
+
 (define (interpret-row row-stx)
   (syntax-parse row-stx
     [({~literal rows}
-      ({~literal repeat} repeat-number:number)
-      ({~literal chunk} chunk-size:number chunk-string:str) ... ";")
+      ({~literal repeat} repeat-number)
+      chunks ... ";")
 
      (for ([i (in-range (syntax-e #'repeat-number))])
-       (for ([n-stx (syntax->list #'(chunk-size ...))]
-             [s-stx (syntax->list #'(chunk-string ...))])
-         (for ([k (syntax-e n-stx)])
-           (display (syntax-e s-stx))))
+       (for ([chunk-stx (syntax->list #'(chunks ...))])
+         (interpret-chunk chunk-stx))
        (newline))]))
 
+
+(define (interpret-chunk chunk-stx)
+  (syntax-parse chunk-stx
+    [({~literal chunk} chunk-size chunk-string)
+
+     (for ([k (syntax-e #'chunk-size)])
+       (display (syntax-e #'chunk-string)))]))
