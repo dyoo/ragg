aa15d8fce370822b1d78d30688abb1347721c6d6
Danny Yoo <dyoo@hashcollision.org>
2013-01-09 15:47:38 -0700

    almost done with second example; need to finish with compilation

--- ragg/examples/simple-line-drawing/interpret.rkt
+++ ragg/examples/simple-line-drawing/interpret.rkt
@@ -17,8 +17,8 @@
       ({~literal chunk} chunk-size:number chunk-string:str) ... ";")
 
      (for ([i (in-range (syntax-e #'repeat-number))])
-       (for* ([n-stx (syntax->list #'(chunk-size ...))]
-              [s-stx (syntax->list #'(chunk-string ...))])
+       (for ([n-stx (syntax->list #'(chunk-size ...))]
+             [s-stx (syntax->list #'(chunk-string ...))])
          (for ([k (syntax-e n-stx)])
            (display (syntax-e s-stx))))
        (newline))]))
