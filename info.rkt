9427bb8f3e0d662459abb3dc2f63bf6e63518603
Asumu Takikawa <asumu@ccs.neu.edu>
2014-01-29 23:07:38 -0500

    Add package dependencies and collection flag
    Needed for compatibility with Racket v6.0

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+
+(define deps '("base" "parser-tools-lib" "rackunit-lib"))
+(define build-deps '("at-exp-lib" "parser-tools-doc" "racket-doc"
+                     "scribble-lib"))
+(define collection 'multi)
+
